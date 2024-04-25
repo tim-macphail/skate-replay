4ab3ed36e620991e153abd55550f1c04d2dd466f
tim-macphail <timothy.macphail@gmail.com>
2024-04-24 17:34:29 -0700

    delay playback and slowmo toggle on keypress

---
+++ main.py
@@ -0,0 +1,52 @@
+import cv2
+
+
+def show_webcam():
+    # Initialize the webcam
+    print("initializing webcam...")
+    cap = cv2.VideoCapture(0)
+    print("webcam initialized")
+
+    max_fps = cap.get(cv2.CAP_PROP_FPS)
+
+    delay_seconds = 1
+    buffer_cap = int(max_fps * delay_seconds)
+    buffer = []
+    slow_mo = False
+    show_frame = True
+
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        buffer.append(frame)
+
+        if len(buffer) > buffer_cap:
+            if show_frame:
+                cv2.imshow("Webcam", buffer.pop(0))
+
+        # Exit the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            print("quitting...")
+            break
+
+        if cv2.waitKey(1) & 0xFF == ord("s"):
+            if not slow_mo:
+                print("okay slow mo now")
+                slow_mo = True
+            else:
+                print("back to regs")
+                slow_mo = False
+                buffer = buffer[:buffer_cap]
+                show_frame = True
+
+        if slow_mo:
+            show_frame = not show_frame
+
+    # Release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    show_webcam()
