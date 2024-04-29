b07268b7225279a1d4a72603954d0f9c227afe2a
tim-macphail <timothy.macphail@gmail.com>
2024-04-29 09:51:29 -0700

    same functionality - refactor

--- main.py
+++ main.py
@@ -4,13 +4,19 @@ import pyaudio
 import numpy as np
 import time
 
+# Configuration
+CLAP_THRESHOLD = 2000  # Adjust this threshold according to your environment
+CLAP_DEBOUNCE_TIME = 0.5  # Debounce time in seconds
+STREAM_DELAY = 0  # seconds
+
 # Constants for audio settings
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
 CHUNK = 1024
-THRESHOLD = 5000  # Adjust this threshold according to your environment
-DEBOUNCE_TIME = 0.5  # Debounce time in seconds
+
+# Variables for debounce
+clap_detected_time = 0
 
 # Initialize PyAudio
 audio = pyaudio.PyAudio()
@@ -20,11 +26,6 @@ stream = audio.open(
     format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK
 )
 
-print("Listening for claps...")
-
-# Variables for debounce
-clap_detected_time = 0
-
 
 # Initialize the webcam
 print("initializing webcam...")
@@ -33,9 +34,8 @@ print("webcam initialized")
 
 max_fps = cap.get(cv2.CAP_PROP_FPS)
 
-delay_seconds = 1
-buffer_cap = int(max_fps * delay_seconds)
-buffer = []
+frame_buffer = []
+buffer_size_cap = int(max_fps * STREAM_DELAY)
 slow_mo = False
 show_frame = True
 
@@ -43,10 +43,11 @@ while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
 
-    buffer.append(frame)
+    frame = cv2.flip(frame, 1)
+    frame_buffer.append(frame)
 
-    if show_frame and len(buffer) > buffer_cap:
-        cv2.imshow("Webcam", buffer.pop(0))
+    if show_frame and len(frame_buffer) > buffer_size_cap:
+        cv2.imshow("Webcam", frame_buffer.pop(0))
 
     # Exit the loop if 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord("q"):
@@ -66,23 +67,26 @@ while True:
         else:
             print("back to regs")
             slow_mo = False
-            buffer = buffer[:buffer_cap]
+            frame_buffer = frame_buffer[:buffer_size_cap]  # truncate the buffer
             show_frame = True
 
     if slow_mo:
-        show_frame = not show_frame
+        show_frame = (
+            not show_frame
+        )  # toggle if we're showing a new frame each loop. TODO: develop a better mechanism for this
 
     # Read audio data from the stream
+    # !this probably takes some time
     data = stream.read(CHUNK)
-    
+
     # Convert binary data to numpy array
     audio_data = np.frombuffer(data, dtype=np.int16)
-    
+
     # Check if the amplitude exceeds the threshold
-    if np.max(np.abs(audio_data)) > THRESHOLD:
+    if np.max(np.abs(audio_data)) > CLAP_THRESHOLD:
         # If a potential clap is detected, check debounce time
         current_time = time.time()
-        if current_time - clap_detected_time > DEBOUNCE_TIME:
+        if current_time - clap_detected_time > CLAP_DEBOUNCE_TIME:
             print("Clap detected!")
             slow_mo = True
             clap_detected_time = current_time
