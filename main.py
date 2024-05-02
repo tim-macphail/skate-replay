340dab5e0c58af9595cfc58efe5341a8b32c94d7
tim-macphail <timothy.macphail@gmail.com>
2024-05-02 09:03:39 -0700

    resume on s

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import pyaudio
 import numpy as np
 
 # configuration constants
-EVENT_AMPLITUDE_THRESHOLD = 5000  # Adjust this threshold according to your environment
-RECOLLECTION = 0.5  # seconds
+EVENT_AMPLITUDE_THRESHOLD = 10000  # Adjust this threshold according to your environment
+RECOLLECTION = 2  # seconds
 CONTINUATION = 1  # seconds
 REPLAY_PLAYBACK_RATE = 0.25
 
@@ -35,28 +35,33 @@ REPLAY_FRAME_COUNT = int(FRAME_RATE * (RECOLLECTION + CONTINUATION))
 
 # global control variables
 memory = []
-slowmo_control = 0
+playback_control = 0
 
 while True:
     ret, frame = cap.read()
     frame = cv2.flip(frame, 1)
+    audio_data = stream.read(CHUNK)
 
     memory.append(frame)
 
-    if not slowmo_control:
+    if not playback_control:
         cv2.imshow("Webcam", frame)
         while len(memory) > MEMORY_CAP:
             memory.pop(0)
 
-        data = stream.read(CHUNK)
-        audio_data = np.frombuffer(data, dtype=np.int16)
+        audio_data = np.frombuffer(audio_data, dtype=np.int16)
         if np.max(np.abs(audio_data)) > EVENT_AMPLITUDE_THRESHOLD:
-            print("event detected!")
-            slowmo_control = REPLAY_FRAME_COUNT * REPLAY_FRAME_INTERVAL
+            print(f"event with amplitude {np.max(np.abs(audio_data))} detected!")
+            playback_control = REPLAY_FRAME_COUNT * REPLAY_FRAME_INTERVAL
     else:
-        if slowmo_control % REPLAY_FRAME_INTERVAL == 0:
+        if playback_control % REPLAY_FRAME_INTERVAL == 0:
             cv2.imshow("Webcam", memory.pop(0))
-        slowmo_control -= 1
+        playback_control -= 1
+        if playback_control == 0:
+            print("resume live stream.")
+
+    if cv2.waitKey(1) & 0xFF == ord("s"):
+        playback_control = 0
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         print("quitting...")
